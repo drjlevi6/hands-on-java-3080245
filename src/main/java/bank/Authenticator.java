14380c593903e837fcf471ef34c8f571022af74d
Jonathan Levi <drjlevi6@aol.com>
2023-08-29 20:03:41 +0000

    Started Authenticator class

--- src/main/java/bank/Authenticator.java
+++ src/main/java/bank/Authenticator.java
@@ -1,5 +1,19 @@
 package bank;
 
+import javax.security.auth.login.LoginException;
+import javax.sql.DataSource;
+
 public class Authenticator {
-  public static Customer login;
+
+  public static Customer login(String username, String password)
+      throws LoginException {
+    Customer customer = DataSource.getCustomer(username);
+    if (customer == null) {
+      throw new LoginException("Username not found");
+    }
+  }
+
+  public static void logout(Customer customer) {
+
+  }
 }
