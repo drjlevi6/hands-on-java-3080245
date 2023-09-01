e72e00266cf341dbc9d9f981f6071de3ac3a35b2
Jonathan Levi <drjlevi6@aol.com>
2023-09-01 20:50:50 +0000

    Account.withdraw throws AmountException

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -46,11 +46,11 @@ public class Account {
     }
   }
 
-  public void withdraw(double amount) {
+  public void withdraw(double amount) throws AmountException {
 
     if (amount < 0) {
       throw new AmountException(
-        "The withdrawal amount must be greater than 0.");
+          "The withdrawal amount must be greater than 0.");
     }
   }
 }
