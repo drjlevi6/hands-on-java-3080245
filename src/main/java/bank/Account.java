ff19ca3e943df6cb893eff508e2c1efb47b43b8d
Jonathan Levi <drjlevi6@aol.com>
2023-09-01 17:37:45 +0000

    Account.deposit hdr. now specifies AmountException

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,5 +1,7 @@
 package bank;
 
+import bank.exceptions.AmountException;
+
 public class Account {
   private int id;
   private String type;
@@ -35,7 +37,7 @@ public class Account {
     this.balance = balance;
   }
 
-  public void deposit(double amount) {
+  public void deposit(double amount) throws AmountException {
     if (amount < 1) {
       throw new AmountException("The minimum deposit us 1.");
     }
